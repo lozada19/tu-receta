34ee716b870b6ad26da16fdb27dbdcc24cc22a63
RoVoth <droxer@hotmail.es>
2022-08-01 11:21:15 +0200

    recipe BD

---
+++ seeds/recipe.seed.js
@@ -0,0 +1,26 @@
+const Recipes = [
+  {
+    recipename: "Tortilla de patata con cebolleta y ajetes",
+    ingredients: [
+      "6 huevos",
+      "600 gramos de patatas",
+      "1 cebolleta",
+      "2 ajetes",
+      "Aceite de oliva",
+      "Sal",
+      "Pimienta",
+      "Hierbas aromáticas al gusto: tomillo, perifollo, cebollino",
+    ],
+    preparation: [
+      "Corta y fríe las patatas. Pela las patatas, lávalas y córtalas en rodajas de ½ cm. Calienta abundante aceite en una sartén honda, añade las patatas y fríelas, a fuego medio, unos 15 minutos.",
+      "Añade los ajetes y la cebolleta. Limpia la cebolleta y los ajetes, lávalos y pícalos finos. Añade ambos a la sartén, baja la intensidad de la llama al mínimo y prosigue la cocción 10 o 12 minutos, removiendo de vez en cuando, hasta que estén transparentes.",
+      "Escurre en un colador. Retira de la sartén con aceite las patatas, la cebolleta y los ajetes, déjalos escurrir en un colador grande y sazona.",
+      "Bate los huevos. Casca los huevos en un cuenco amplio, salpimiéntalos y bátelos con un tenedor. Agrega las verduras escurridas, mezcla y deja reposar 5 minutos.",
+      "Cuela el aceite. Mientras, lava y trocea las hierbas elegidas. Cuela el aceite de freír las verduras, añade 2 cucharadas a una sartén antiadherente y ponla al fuego. Vierte la preparación anterior y extiéndela de modo que cubra toda la base.",
+      "Cuaja la tortilla. Cuaja la tortilla 3 o 4 minutos a fuego medio, moviendo la sartén con suavidad para que no se pegue. Dale la vuelta y hazla 2 o 3 minutos por el otro lado.",
+      "Sírvela cortada. Sirve la tortilla cortada en triángulos o en dados pequeños, y decorada con unas hojitas de las hierbas elegidas encima de cada pedazo.",
+    ],
+    image: "",
+    owner: "",
+  },
+];
