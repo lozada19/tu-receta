2b44e26efa057b8c5e39f96224cb06164cae8439
RoVoth <droxer@hotmail.es>
2022-08-02 12:34:16 +0200

    profile done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,18 @@
-function isLoggedIn(req, res, next)  {
-    
-    if (req.session.user === undefined) {
-        res.redirect("/auth/login")
-    }else {
-        next()
-    }
+function isLoggedIn(req, res, next) {
+  if (req.session.user === undefined) {
+    res.redirect("/auth/login");
+  } else {
+    next();
+  }
 }
 
 function localsUpdate(req, res, next) {
-    if (req.session.user === undefined) {
-      res.locals.isUserActive = false;
-    } else if (req.session.user !== undefined) {
-      res.locals.isUserActive = true;
-    }
-    next();
+  if (req.session.user === undefined) {
+    res.locals.isUserActive = false;
+  } else if (req.session.user !== undefined) {
+    res.locals.isUserActive = true;
   }
-  
+  next();
+}
+
 module.exports = { isLoggedIn, localsUpdate };
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,9 +1,17 @@
 const router = require("express").Router();
 
-const {isLoggedIn} = require("../middlewares/auth.js");
+const { isLoggedIn } = require("../middlewares/auth.js");
+const RecipeModel = require("../models/Recipe.model.js");
 
-router.get("/", isLoggedIn, (req, res, next) => {
-  res.render("profile/profile");
+router.get("/", (req, res, next) => {
+  // const { myRecipeId } = req.session.user._id;
+  RecipeModel.find({ owner: req.session.user._id })
+    .then((myRecipe) => {
+      res.render("profile/profile.hbs", { myRecipe });
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
